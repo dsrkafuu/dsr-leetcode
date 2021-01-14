24e9be581d500a0ce6ce31da8eb1d92129f0919a
amzrk2 <amzrk2@outlook.com>
2021-01-14 15:47:17 +0800

    new: lcof 05

---
+++ lcof/05/ans.js
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+ *
+ * 替换空格
+ *
+ * 请实现一个函数，把字符串 s 中的每个空格替换成 "%20"。
+ *
+ * 限制：
+ *
+ * 0 <= s 的长度 <= 10000
+ *
+ * https://leetcode-cn.com/submissions/detail/138350073/
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const replaceSpace = (s) => s.replace(/ /gi, '%20');
