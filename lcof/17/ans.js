2626612e2f4cad7ed57c9c32c93cb7353733aa0c
amzrk2 <amzrk2@outlook.com>
2021-01-21 11:10:53 +0800

    new: lcof 18

---
+++ lcof/17/ans.js
@@ -0,0 +1,41 @@
+/**
+ * https://leetcode-cn.com/problems/da-yin-cong-1dao-zui-da-de-nwei-shu-lcof/
+ *
+ * 打印从 1 到最大的 n 位数
+ *
+ * 输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。
+ * 比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
+ *
+ * 示例:
+ * 输入: n = 1
+ * 输出: [1,2,3,4,5,6,7,8,9]
+ *
+ * 限制：
+ * n 为正整数
+ *
+ * https://leetcode-cn.com/submissions/detail/139994688/
+ */
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+const printNumbers = (n) => {
+  const res = [];
+  let max = 10 ** n - 1;
+  if (Number.isSafeInteger(max)) {
+    for (let i = 1; i <= max; i++) {
+      res.push(i);
+    }
+  } else {
+    max = BigInt(new Array(n).fill('9').join(''));
+    for (let i = 1n; i <= max; i++) {
+      res.push(i);
+    }
+  }
+  return res;
+};
+
+// DEBUG
+console.log(printNumbers(2));
+console.log(printNumbers(60));
---
+++ lcof/18/ans.js
@@ -0,0 +1,55 @@
+/**
+ * https://leetcode-cn.com/problems/shan-chu-lian-biao-de-jie-dian-lcof/
+ *
+ * 删除链表的节点
+ *
+ * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+ * 返回删除后的链表的头节点。
+ *
+ * 限制：
+ * 题目保证链表中节点的值互不相同
+ *
+ * https://leetcode-cn.com/submissions/detail/139998699/
+ */
+import { ListNode, List } from '../../class/List.js';
+
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+const deleteNode = (head, val) => {
+  // 空链表
+  if (!head) {
+    return null;
+  }
+  // 删除头
+  if (head.val === val) {
+    head = head.next;
+    return head;
+  }
+
+  // 工作指针
+  let fast = head.next;
+  let slow = head;
+  while (fast) {
+    if (fast.val === val) {
+      fast = fast.next;
+      slow.next = fast;
+      return head;
+    }
+    slow = slow.next;
+    fast = fast.next;
+  }
+  return head;
+};
+
+// DEBUG
+const list1 = new List([3]);
+console.log(deleteNode(list1.head, 3));
+const list2 = new List([3, 4]);
+console.log(deleteNode(list2.head, 3));
+const list3 = new List([3, 4]);
+console.log(deleteNode(list3.head, 4));
+const list4 = new List([3, 4, 5, 6, 7]);
+console.log(deleteNode(list4.head, 6));
